f8184b429e3b441ce9065a995dd2f4af1c6ec934
Evans <evaslangat@gmail.com>
2026-04-10 17:23:33 +0300

    first commit

--- src/main/java/com/Jakima_Estate/JakimaApplication.java
+++ src/main/java/com/Jakima_Estate/JakimaApplication.java
@@ -1,5 +1,7 @@
 package com.Jakima_Estate;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,14 @@ public class JakimaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JakimaApplication.class, args);
+	}
+
+	@Value("${spring.datasource.url}")
+	private String dbUrl;
+
+	@PostConstruct
+	public void printDb() {
+		System.out.println("DB URL: " + dbUrl);
 	}
 
 }
